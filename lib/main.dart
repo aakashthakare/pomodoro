7b3b5e3ec3257825982a20687d0aa07b7359b596
Akash <akash.thakare@thoughtworks.com>
2024-05-16 18:52:43 +0530

    Style improvement and refactoring.

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,9 @@ class PomodoroTimer extends StatefulWidget {
 class PomodoroSession {
   String title;
   int seconds;
+  Color color;
 
-  PomodoroSession(this.title, this.seconds);
+  PomodoroSession(this.title, this.seconds, this.color);
 }
 
 class PomodoroSessionManager {
@@ -65,23 +66,31 @@ class PomodoroSessionManager {
   next() {
     int timerMinutes;
     String title;
+    Color color;
 
     if (state == end) {
       state = start;
       timerMinutes = longBreak;
       title = "Long Break";
+      color = Colors.blueGrey;
     } else {
       if (state % 2 != 0) {
         timerMinutes = session;
-        title = "Work In Progress";
+        title = "Focus";
+        color = Colors.lightGreen;
       } else {
         timerMinutes = smallBreak;
-        title = "Small Break";
+        color = Colors.lightBlue;
+        title = "Break";
       }
       state++;
     }
+    return PomodoroSession(
+        title, Duration(minutes: timerMinutes).inSeconds, color);
+  }
 
-    return PomodoroSession(title, timerMinutes); //TODO MUltiply with * 60
+  clear() {
+    state = 1;
   }
 }
 
@@ -112,54 +121,74 @@ class PomodoroTimerState extends State<PomodoroTimer> {
     var minutes = (duration.inMinutes % 60).toString().padLeft(2, '0');
     var seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
 
-    return Center(
-        child: Column(children: [
-      Text(
-        session.title,
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
-      ),
-      Text(
-        "$hours:$minutes:$seconds",
-        style: TextStyle(fontSize: 120, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
-      ),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        ElevatedButton(
-            onPressed: _isStarted ? null : start,
-            style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(20),
-                backgroundColor: Colors.white, // <-- Button color
-                foregroundColor: Colors.lightBlue),
-            child: const Icon(
-              Icons.play_circle,
-              size: 50,
-            )),
-        ElevatedButton(
-            onPressed: _isStarted ? pause : null,
-            style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(20),
-                backgroundColor: Colors.white, // <-- Button color
-                foregroundColor: Colors.lightBlue),
-            child: const Icon(
-              Icons.pause,
-              size: 50,
-            )),
-        ElevatedButton(
-            onPressed: reset,
-            style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(20),
-                backgroundColor: Colors.white, // <-- Button color
-                foregroundColor: Colors.lightBlue),
-            child: const Icon(
-              Icons.restore,
-              size: 50,
-            ))
-      ])
-    ]));
+    String timer;
+
+    if (hours == "00") {
+      timer = "$minutes:$seconds";
+    } else {
+      timer = "$hours:$minutes:$seconds";
+    }
+
+    return Container(
+        color: session.color,
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              Text(
+                timer,
+                style: TextStyle(
+                    fontSize: 124,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                session.title,
+                style: TextStyle(
+                    fontSize: 36,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 100),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton(
+                    onPressed: _isStarted ? null : start,
+                    style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20),
+                        backgroundColor: Colors.white, // <-- Button color
+                        foregroundColor: Colors.lightBlue),
+                    child: const Icon(
+                      Icons.play_circle,
+                      size: 50,
+                    )),
+                ElevatedButton(
+                    onPressed: _isStarted ? pause : null,
+                    style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20),
+                        backgroundColor: Colors.white, // <-- Button color
+                        foregroundColor: Colors.lightBlue),
+                    child: const Icon(
+                      Icons.pause,
+                      size: 50,
+                    )),
+                ElevatedButton(
+                    onPressed: reset,
+                    style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20),
+                        backgroundColor: Colors.white, // <-- Button color
+                        foregroundColor: Colors.lightBlue),
+                    child: const Icon(
+                      Icons.restore,
+                      size: 50,
+                    ))
+              ])
+            ])));
   }
 
   start() {
@@ -180,8 +209,9 @@ class PomodoroTimerState extends State<PomodoroTimer> {
     setState(() {
       pause();
       _isStarted = false;
-      updateDuration();
+      sessionManager.clear();
       session = sessionManager.next();
+      updateDuration();
     });
   }
 
@@ -193,9 +223,8 @@ class PomodoroTimerState extends State<PomodoroTimer> {
     setState(() {
       session.seconds -= 1;
       if (session.seconds == 0) {
-        reset();
+        session = sessionManager.next();
         ringTheBell();
-        start();
       }
       updateDuration();
     });
